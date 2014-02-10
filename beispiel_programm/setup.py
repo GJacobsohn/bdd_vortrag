7c4c5dad98efa4527a4e3842ad795ebcbe71b554
Gabriel <gaja@akra.de>
2014-02-10 11:34:10 +0100

    Added setup.py and clean db file

--- beispiel_programm/setup.py
+++ beispiel_programm/setup.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distribute_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
 
 setup(name='HiperCRM Vortrags prog',
       version='0.1',
-      description='Dies ist ein kleines Djaqngo Projekt um BDD zu ',
+      description='Ein kleines Django-Projekt um BDD zu demonstrieren',
       author='Gabriel Jacobsohn',
       author_email='gaja@ara.de',
       url='https://github.com/GJacobsohn/bdd_vortrag',
-      install_requires=["Django<1.6",
-                "django-nose==1.2",
-                "lettuce==0.2.19",
-                "nose==1.3.0",
-                "selenium==2.39.0"],
+      install_requires=["Django<1.5.5",
+                        "django-nose==1.2",
+                        "lettuce==0.2.19",
+                        "nose==1.3.0",
+                        "selenium==2.39.0"],
      )
